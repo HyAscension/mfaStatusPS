268ffff720da8bfc55dc60ce6935a75287860d06
Hy Vo <94402902+HyAscension@users.noreply.github.com>
2023-04-05 11:19:00 -0500

    Update mfaEnables.ps1
    Display authentication details and export.

--- mfaEnables.ps1
+++ mfaEnables.ps1
@@ -3,3 +3,36 @@ $Users = Get-MsolUser -All | ? { $_.UserType -ne  "Guest" }
 # Output file
 $Report = [System.Collections.Generic.List[Object]]::new()
 Write-Host "Processing" $Users.Count "accounts..."
+ForEach ($User in $Users) {
+    $MFAEnforced = $User.StrongAuthenticationRequirements.State
+    $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber
+    $DefaultMFAMethod = ($User.StrongAuthenticationMethods | ? { $_.IsDefault -eq "True" }).MethodType
+    If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) {
+        Switch ($DefaultMFAMethod) {
+            "OneWaySMS" { $MethodUsed = "One-way SMS" }
+            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" }
+            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" }
+            "PhoneAppNotification" { $MethodUsed = "Authenticator app" }
+        }
+    }
+    Else {
+        $MFAEnforced = "Not Enabled"
+        $MethodUsed = "MFA Not Used"
+    }
+     
+    $ReportLine = [PSCustomObject] @{
+        User        = $User.UserPrincipalName
+        Name        = $User.DisplayName
+        Department  = $User.Department
+        JobTitle    = $User.JobTitle
+        MFAUsed     = $MFAEnforced
+        MFAMethod   = $MethodUsed
+        PhoneNumber = $MFAPhone
+    }
+     
+        $Report.Add($ReportLine)
+}
+     
+Write-Host "Report is in (path)"
+$Report | Select-Object User, Name, Department, JobTitle, MFAUsed, MFAMethod, PhoneNumber | Sort Name | Out-GridView
+$Report | Sort Name | Export-CSV -NoTypeInformation -Encoding UTF8 #path
