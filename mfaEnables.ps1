c4c6896f3f98cdeb4ab56730316cfe3920293e27
Hy Vo <94402902+HyAscension@users.noreply.github.com>
2023-04-06 10:17:00 -0500

    Update mfaEnables.ps1
    Include additional authentication methods

--- mfaEnables.ps1
+++ mfaEnables.ps1
@@ -1,18 +1,30 @@
 Connect-MsolService
-$Users = Get-MsolUser -All | ? { $_.UserType -ne  "Guest" }
+
+# Search all users
+$Users = Get-MsolUser -All | Where-Object { $_.UserType -ne  "Guest" }
+
 # Output file
 $Report = [System.Collections.Generic.List[Object]]::new()
 Write-Host "Processing" $Users.Count "accounts..."
+
 ForEach ($User in $Users) {
     $MFAEnforced = $User.StrongAuthenticationRequirements.State
     $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber
     $DefaultMFAMethod = ($User.StrongAuthenticationMethods | ? { $_.IsDefault -eq "True" }).MethodType
+    
     If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) {
         Switch ($DefaultMFAMethod) {
             "OneWaySMS" { $MethodUsed = "One-way SMS" }
-            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" }
-            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" }
+            "TwoWayVoiceMobile" { $MethodUsed = "Phone call" }
+            "PhoneAppOTP" { $MethodUsed = "Authenticator app" }
             "PhoneAppNotification" { $MethodUsed = "Authenticator app" }
+            
+            # Include additional authentication methods
+            "TwoWayVoiceOffice" { $MethodUsed = "Phone call" }
+            "PhoneApp" { $MethodUsed = "Authenticator app" }
+            "HardwareToken" { $MethodUsed = "Hardware token" }
+            "Email" { $MethodUsed = "Email" }
+            "Unknown" { $MethodUsed = "Unknown" }
         }
     }
     Else {
