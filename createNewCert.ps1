2a7a078355bf242969c37643e23dd6431e1d1d2e
Hy Vo <94402902+HyAscension@users.noreply.github.com>
2023-04-26 11:55:00 -0500

    Update createNewCert.ps1
    Create new self-signed certificate and export it

---
+++ createNewCert.ps1
@@ -0,0 +1,3 @@
+$certname = "{certificateName}"
+$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+Export-Certificate -Cert $cert -FilePath "Path\$certname.cer"   ## Specify your preferred location
